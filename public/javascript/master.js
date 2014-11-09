315c0661a1bde327c35083af0d049b66a9af2ae4
Kyle Chadha <kyle.chadha@buzzfeed.com>
2014-11-09 12:47:24 -0500

    Add custom date/time and number of people to index view

--- public/javascript/master.js
+++ public/javascript/master.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
+  $('#datetimepicker1').datetimepicker({
+      defaultDate: "11/14/2014 7:00 PM",
+      minuteStepping:30               //set the minute stepping
+  });
+
   $('.btn-scrape').on('click', function() {
     $('.btn-text').hide();
     $('.spinner').show();
   })
+
 });
