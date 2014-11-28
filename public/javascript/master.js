fcd45da7ddca19aeaf6635c58560bb25e5d06e15
Kyle Chadha <kyle.chadha@buzzfeed.com>
2014-11-28 10:40:16 -0500

    Update time picker to start on the current date / prevent no results error

--- public/javascript/master.js
+++ public/javascript/master.js
@@ -5,11 +5,14 @@
 // Normally we would create modules to match our component structure, but in the interest of time / simplicity we haven't here.
 $(document).ready(function() {
   
+  // Pull the current date and set the default time to be 7:30PM.
   var now = moment();
+  now.hour(19);
+  now.minute(30);
 
   // Configure the date and time picker -- increments of 30 and present forward only.
   $('#datetimepicker1').datetimepicker({
-      defaultDate: "11/14/2014 7:30 PM",
+      defaultDate: now,
       minDate: now,
       minuteStepping: 30
   });
